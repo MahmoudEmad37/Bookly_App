5cf92157a03f3d9ebf75a6e8ed8a93389121997b
Mahmoud Emad <mahmoudemad3773@gmail.com>
2024-04-22 16:18:15 +0200

    create featured books state

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,6 @@ import 'package:bookly/core/resourses/routes_manager.dart';
 import 'package:bookly/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 
 void main() async {
